ac59b43e7da47085c1e22b4b000a3fb6e09c6347
user7d11 <user7w11@gmail.com>
2022-06-13 07:32:43 +0200

    Second commit

--- js/script.js
+++ js/script.js
@@ -1,69 +1,95 @@
 const API_KEY = 'EE9PYLn8oYA-qxW3SQYq';
 
-let startDate
-let endDate
-let firstStart = false;
+let startDate = '';
+let endDate = '';
+const productBitcoin = 0;
+const productGold = 0;
+let url;
+let id;
+let chartValuesX = [];
+let chartValuesY = [];
+let chartValuesMin;
+let chartValuesMax;
 
-async function loadCourseCurrently() {
-    startDate = new Date().toISOString().split('T')[0]; //'2022-05-22'
-    endDate = new Date().toISOString().split('T')[0];
+// async function loadCourseCurrently() {
+//     let today = new Date();
+//     today.setDate(new Date().getDate() - 2);
+//     let startDateLastDay = today.toISOString().split('T')[0];
 
-    loadCourse();
-    input2();
+//     startDate = startDateLastDay;
+
+//     // startDate = new Date().toISOString().split('T')[0]; //'2022-05-22'
+//     endDate = new Date().toISOString().split('T')[0];
+
+//     id = 1;
+//     loadCourse();
+// }
+
+function changeProduct(num) {
+    id = num;
+    startDate = document.getElementById('startDate').value;
+    endDate = document.getElementById('endDate').value;
+
+    if (!startDate == '' && !endDate == '') {
+        document.getElementById('diagram1').classList.add('d-none');
+        document.getElementById('diagram2').classList.add('d-none');
+        document.getElementById('myChart').classList.remove('d-none');
+
+        loadCourse();
+    }
 }
 
-function input2() {
-    document.getElementById('startDate').placeholder = "10.06.2022";
-    document.getElementById('endDate').placeholder = "10.06.2022";
-    console.log(startDate, endDate)
+function renderChangeProduct() {
+    if (id == 0) {
+        url = `https://data.nasdaq.com/api/v3/datasets/BCHAIN/MKPRU?start_date=${startDate}&end_date=${endDate}&api_key=${API_KEY}`;
+    }
+    if (id == 1) {
+        url = `https://data.nasdaq.com/api/v3/datasets/LBMA/GOLD?start_date=${startDate}&end_date=${endDate}&api_key=${API_KEY}`;
+    }
 }
 
 async function loadCourse() {
-    // https://data.nasdaq.com/api/v3/datasets/BCHAIN/MKPRU?start_date='2022-05-22'&end_date='2022-05-22'&api_key=EE9PYLn8oYA-qxW3SQYq
-    // let url = `https://data.nasdaq.com/api/v3/datasets/BITFINEX/LUNAF0USTF0?start_date=${startDate}&end_date=${endDate}&api_key=${API_KEY}`;
-    let url = `https://data.nasdaq.com/api/v3/datasets/BCHAIN/MKPRU?start_date=${startDate}&end_date=${endDate}&api_key=${API_KEY}`;
+    renderChangeProduct();
 
     let response = await fetch(url);
-    let responseAsJson = await response.json();
-    let bitcoinDate = responseAsJson.dataset.data;
+    let responseAsJson = await response.json(url);
+    let courseDateApi = responseAsJson.dataset.data;
+    // console.log(responseAsJson)
 
-    if (!firstStart) { showBitcoinToday(bitcoinDate); };
-    renderTable(bitcoinDate);
-    dateCalculat(bitcoinDate);
+    renderTable(courseDateApi);
+    currencyChart(courseDateApi);
+    dateChart(courseDateApi);
     myChart();
-    xValues = [];
-    yValues = [];
+    chartValuesX = [];
+    chartValuesY = [];
+    textCourseUpdate();
+    valueFieldColor();
 }
 
-function dateFormatDE(index){
+function dateFormatDE(index) {
     let options = { day: '2-digit', month: '2-digit', year: 'numeric' };
     let date = new Date(index);
     date = date.toLocaleDateString('de-DE', options);
     return date;
 }
 
-function showBitcoinToday(bitcoinDate) {
-    document.getElementById('showBitcoinToday').innerHTML = bitcoinDate[0][1];
-
-    let date = dateFormatDE(bitcoinDate[0][0]);
-
-    document.getElementById('showDateToday').innerHTML = date;
-    firstStart = true;
+function textCourseUpdate() {
+    let nameCourse;
+    if (id == 0) { nameCourse = 'Bitcoin' };
+    if (id == 1) { nameCourse = 'Gold' };
+    document.getElementById('textCourseUpdate').innerHTML = `<b>${nameCourse}</b> Kurs <br><nobr>max. <b>${chartValuesMax} USD</b> <wbr>min. <b>${chartValuesMin} USD</b></nobr>`;
 }
 
-function showCourse() {
-    startDate = document.getElementById('startDate').value;
-    endDate = document.getElementById('endDate').value;
+// change date - button
+// async function formValidation() {
+//     startDate = await document.getElementById('startDate').value;
+//     endDate = await document.getElementById('endDate').value;
 
-    loadCourse();
-    // myChart();
-    // xValues = [];
-    // yValues = [];
-}
+//     loadCourse();
+// }
 
-function renderTable(bitcoinDate) {
+function renderTable(courseDateApi) {
     let table = document.getElementById('tabelCourse');
-
     table.innerHTML = '';
 
     table.innerHTML +=
@@ -74,32 +100,46 @@ function renderTable(bitcoinDate) {
             </tr>
         `;
 
-
-    for (let i = 0; i < bitcoinDate.length; i++) {
-        const element = bitcoinDate[i];
-
+    for (let i = 0; i < courseDateApi.length; i++) {
+        const element = courseDateApi[i];
         let date = dateFormatDE(element[0]);
 
         table.innerHTML +=
             `
             <tr>
                 <td>${date}</td>
-                <td>${element[1]}</td>
+                <td id="valueFieldColor${element[1].toFixed(0)}">${element[1].toFixed(2)} USD</td>
             </tr>
         `;
     }
 }
 
-var xValues = [];
-var yValues = [];
+// 
+function valueFieldColor() {
+    document.getElementById('valueFieldColor' + (chartValuesMin + 1)).style.backgroundColor = '#ffbaba';
+    document.getElementById('valueFieldColor' + (chartValuesMax - 1)).style.backgroundColor = '#bdbdff';
+}
 
-function dateCalculat(bitcoinDate) {
-    for (let i = 0; i < bitcoinDate.length; i++) {
-        const element = bitcoinDate[i];
+// ---------- myChart
+
+function currencyChart(courseDateApi) {
+    let index = [];
+    for (let i = 0; i < courseDateApi.length; i++) {
+        const element = courseDateApi[i];
+
+        index.push(element[1].toFixed(0))
+    }
+    chartValuesMin = Math.min(...index) - 1;
+    chartValuesMax = Math.max(...index) + 1;
+}
+
+function dateChart(courseDateApi) {
+    for (let i = 0; i < courseDateApi.length; i++) {
+        const element = courseDateApi[i];
 
         let date = dateFormatDE(element[0]);
-        xValues.push(date);
-        yValues.push(element[1]);
+        chartValuesX.push(date);
+        chartValuesY.push(element[1]);
     }
 }
 
@@ -107,28 +147,98 @@ function myChart() {
     new Chart("myChart", {
         type: "line",
         data: {
-            labels: xValues,
+            labels: chartValuesX,
             datasets: [{
                 fill: false,
                 lineTension: 0,
                 backgroundColor: "rgba(0,0,255,1.0)",
                 borderColor: "rgba(0,0,255,0.1)",
-                data: yValues
+                data: chartValuesY
             }]
         },
         options: {
             legend: { display: false },
             scales: {
-                yAxes: [{ ticks: { min: 0, max: 50000 } }],
+                yAxes: [{ ticks: { min: chartValuesMin, max: chartValuesMax } }],
             }
         }
     });
 }
 
+// ---------- navigation menu
 
+function navBar() {
+    document.getElementById('body').classList.toggle('overflow');
 
+    document.getElementById('menu').classList.toggle('d-none');
+    document.getElementById('barImg').classList.toggle('barImg');
 
-// Beispiel Aktuelles Datum - ein Tag davor
-let today = new Date();
-today.setDate(new Date().getDate() - 1);
-let startDateLastDay = today.toISOString().split('T')[0];
+    document.getElementById('pageLaws').classList.toggle('d-none');
+    toggleRegulations('imprint', 'privacy', 'copyright');
+}
+
+function backNavBar() {
+    document.getElementById('menu').classList.toggle('d-none');
+    document.getElementById('barImg').classList.toggle('barImg');
+}
+
+function back() {
+    document.getElementById('body').classList.toggle('overflow');
+    document.getElementById('pageLaws').classList.toggle('d-none');
+    document.getElementById('backNavBar').classList.add('d-none');
+    document.getElementById('back').classList.add('d-none');
+    toggleRegulations('imprint', 'privacy', 'copyright');
+}
+
+// ---------- jump window top
+
+function arrowUp() {
+    window.scrollTo(0, 0);
+}
+
+window.onscroll = function () {
+    let scroll = Math.trunc(window.scrollY);
+
+    if (scroll < 120) {
+        document.getElementById('arrowUp').style.display = 'none';
+    }
+    if (scroll > 120) {
+        document.getElementById('arrowUp').style.display = 'flex';;
+    }
+}
+
+// ---------- page Laws
+
+function imprint() {
+    regulationShow('imprint');
+    toggleRegulations('privacy', 'copyright');
+}
+
+function privacy() {
+    regulationShow('privacy');
+    toggleRegulations('imprint', 'copyright');
+}
+
+function copyright() {
+    regulationShow('copyright');
+    toggleRegulations('imprint', 'privacy');
+}
+
+function regulationShow(index) {
+    window.scrollTo(0, 0);
+    document.getElementById(index).classList.remove('d-none');
+    hideMenu();
+}
+
+function toggleRegulations(first, second, third) {
+    document.getElementById(first).classList.add('d-none');
+    document.getElementById(second).classList.add('d-none');
+    if (third == 'copyright') { document.getElementById(third).classList.add('d-none'); }
+}
+
+function hideMenu() {
+    document.getElementById('back').classList.remove('d-none');
+    document.getElementById('backNavBar').classList.remove('d-none');
+    document.getElementById('menu').classList.toggle('d-none');
+    document.getElementById('barImg').classList.toggle('barImg');
+}
